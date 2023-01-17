c2ef626de7673e3ee52a09dd4c8b4dc13660f392
eyuel gebreselassie <eyuelmulugeta05@gmail.com>
2023-01-17 01:52:10 -0500

    activeted state management

--- src/user.js
+++ src/user.js
@@ -2,17 +2,15 @@ import { createComponent } from "../framework";
 import { div } from "../framework/element";
 import { onClick } from "../framework/event";
 
-// initial state of the User component
-const initialState = { firstName: "Marvin", lastName: "Frachet" };
-
-// state mutating actions
 const methods = {
   changeName: (state, firstName) => ({ ...state, firstName }),
 };
 
-// component template
-const template = ({ firstName, lastName }) =>
-  div`${onClick(() => alert(firstName))} Hello ${firstName} ${lastName}`;
+const initialState = { firstName: "eyuel", lastName: "muler" };
 
-// create a real world component
+const template = ({ firstName, lastName, methods }) =>
+  div`${onClick(() =>
+    methods.changeName("mulugeta")
+  )} Hello ${firstName} ${lastName}`;
+
 export const User = createComponent({ template, methods, initialState });
