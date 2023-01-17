bb6a8517fec7e0582ce1c052d5e51eb9559a9927
eyuel gebreselassie <eyuelmulugeta05@gmail.com>
2023-01-16 19:00:32 -0500

    added virtual dom using snabbdom

--- framework/element.js
+++ framework/element.js
@@ -1,10 +1,24 @@
-const createElement = (tagName) => (strings, ...args) => ({
-  type: tagName,
-  template: strings.reduce(
-    (acc, currentString, index) => acc + currentString + (args[index] || ""),
-    ""
-  ),
+import { h } from "snabbdom/h";
+
+// extract initial value with a template key, some other will appear next ;)
+const initialState = {
+  template: "",
+};
+
+// extract this outside the createElement function
+const createReducer = (args) => (acc, currentString, index) => ({
+  ...acc,
+  template: acc.template + currentString + (args[index] || ""),
 });
+
+const createElement = (tagName) => (strings, ...args) => {
+  const { template } = strings.reduce(createReducer(args), initialState);
+
+  return {
+    type: "element",
+    template: h(tagName, {}, template),
+  };
+};
 
 export const div = createElement("div");
 export const p = createElement("p");
